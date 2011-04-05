1c3f78c96cf06ae70dbebc4ca692aed41a190c29
Matt Sparks <ms@quadpoint.org>
2011-04-05 00:10:37 -0700

    Fixing RangeException call

--- src/packet.cc
+++ src/packet.cc
@@ -4,10 +4,8 @@
 
 uint8_t*
 Packet::offsetAddress(unsigned int offset) const {
-  if (buffer_offset_ + offset >= buffer_->len()) {
-    char *msg = (char *)"Buffer index out of bounds.";
-    throw Fwk::RangeException(__FILE__, __LINE__, msg);
-  }
+  if (buffer_offset_ + offset >= buffer_->len())
+    throw Fwk::RangeException("offsetAddress", "Buffer index out of bounds");
 
   return buffer_->data() + buffer_offset_ + offset;
 }
