7316dfe29865043da64813307d0e4403b3fb216d
Matt Sparks <ms@quadpoint.org>
2011-04-05 00:35:30 -0700

    Making ARPPacket an entity type

--- src/arp_packet.h
+++ src/arp_packet.h
@@ -2,14 +2,24 @@
 #define ARP_PACKET_H_WDIREBCF
 
 #include "fwk/buffer.h"
+#include "fwk/ptr.h"
 
 #include "packet.h"
 
 
 class ARPPacket : public Packet {
  public:
+  typedef Fwk::Ptr<const ARPPacket> PtrConst;
+  typedef Fwk::Ptr<ARPPacket> Ptr;
+
+  static Ptr ARPPacketNew(Fwk::Buffer::Ptr buffer,
+                          unsigned int buffer_offset) {
+    return new ARPPacket(buffer, buffer_offset);
+  }
+
+ protected:
   ARPPacket(Fwk::Buffer::Ptr buffer, unsigned int buffer_offset)
-      : Packet(buffer, buffer_offset) {}
+      : Packet(buffer, buffer_offset) { }
 };
 
 #endif
