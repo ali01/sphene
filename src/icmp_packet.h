291e57136a27221ae2acf786ea23f30eadebe9a8
Ali Yahya <ali01@ali01.com>
2011-04-05 00:48:31 -0700

    Converted icmp_packet to entity type.

--- src/icmp_packet.h
+++ src/icmp_packet.h
@@ -2,13 +2,22 @@
 #define ICMP_PACKET_H_7H1D49WV
 
 #include "fwk/buffer.h"
+using Fwk::Buffer;
 
 #include "packet.h"
 
 
 class ICMPPacket : public Packet {
  public:
-  ICMPPacket(Fwk::Buffer::Ptr buffer, unsigned int buffer_offset)
+  typedef Fwk::Ptr<const ICMPPacket> PtrConst;
+  typedef Fwk::Ptr<ICMPPacket> Ptr;
+
+  static Ptr ICMPPacketNew(Buffer::Ptr buffer, unsigned int buffer_offset) {
+    return new ICMPPacket(buffer, buffer_offset);
+  }
+
+ protected:
+  ICMPPacket(Buffer::Ptr buffer, unsigned int buffer_offset)
       : Packet(buffer, buffer_offset) {}
 };
 
