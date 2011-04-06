e9f15a03f331561308bb83292c61edddfa6964e8
Ali Yahya <ali01@ali01.com>
2011-04-05 22:56:00 -0700

    Added ip_packet_unittest.
    Includes changes to Makefile.am and trivial IP packet version test.

---
+++ tests/ip_packet_unittest.cc
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+
+#include <cstring>
+#include <inttypes.h>
+
+#include "fwk/buffer.h"
+#include "ip_packet.h"
+
+
+class IPPacketTest : public ::testing::Test {
+ protected:
+  IPPacketTest() {
+    /* Sample IP packet buffer in network byte order */
+    uint8_t packet_buffer[] = { 0xAA, 0xBB, 0xCC, /* irrelevant bytes before */
+                               0x45, 0x00, 0x00, 0x78, /* first word */
+                               0x7c, 0x83, 0x00, 0x00, /* second word */
+                               0x6f, 0x11, 0xaf, 0xdc, /* third word */
+                               0x8d, 0x59, 0xe2, 0x92, /* fourth word */
+                               0xab, 0x42, 0x03, 0xe7, /* fifth word */
+                               0xDD, 0xEE, 0xFF /* irrelevant bytes after */ };
+
+    Fwk::Buffer::Ptr buf_ =
+      Fwk::Buffer::BufferNew(packet_buffer, sizeof(packet_buffer));
+    pkt_ = IPPacket::IPPacketNew(buf_, 3);
+  }
+
+  IPPacket::Ptr pkt_;
+};
+
+TEST_F(IPPacketTest, Version) {
+  IPVersion version = pkt_->version();
+  EXPECT_EQ(version, 4);
+}
