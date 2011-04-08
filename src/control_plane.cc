d7542e739933afe59be903218c52f3a6150d23ae
Matt Sparks <ms@quadpoint.org>
2011-04-08 03:26:31 -0700

    Respond to ARP requests

--- src/control_plane.cc
+++ src/control_plane.cc
@@ -30,12 +30,56 @@ ControlPlane::PacketFunctor::PacketFunctor(ControlPlane* const cp)
 
 void ControlPlane::PacketFunctor::operator()(ARPPacket* const pkt,
                                              const Interface::PtrConst iface) {
+  // TODO(ms): Validate all of the fields in the ARP packet.
+
   DLOG << "ARPPacket dispatch in ControlPlane";
   DLOG << "  operation: " << pkt->operationName();
   DLOG << "  sender HW addr: " << (string)pkt->senderHWAddr();
   DLOG << "  sender IP addr: " << (string)pkt->senderPAddr();
   DLOG << "  target HW addr: " << (string)pkt->targetHWAddr();
   DLOG << "  target IP addr: " << (string)pkt->targetPAddr();
+
+  // TODO(ms): Look in ARP cache. If the sender IP address is already in the
+  //   ARP cache, then update the sender hardware address field of the entry in
+  //   the ARP cache and set the merge_flag.
+  //...
+
+  // Are we the target of the ARP packet?
+  if (cp_->dataPlane()->interfaceMap()->interfaceAddr(pkt->targetPAddr())) {
+    DLOG << "we are the target of this ARP packet";
+
+    // TODO(ms): Add the <sender IP, sender HW> to the ARP cache if it wasn't
+    //   there already.
+    //...
+
+    // Look at the opcode.
+    if (pkt->operation() == ARPPacket::kRequest) {
+      DLOG << "sending ARP reply";
+
+      // Swap the hardware and protocol fields.
+      EthernetAddr our_hwaddr = pkt->targetHWAddr();
+      IPv4Addr our_ipaddr = pkt->targetPAddr();
+      pkt->targetHWAddrIs(pkt->senderHWAddr());
+      pkt->targetPAddrIs(pkt->senderPAddr());
+      pkt->senderHWAddrIs(our_hwaddr);
+      pkt->senderPAddrIs(our_ipaddr);
+
+      // Flip the operation.
+      pkt->operationIs(ARPPacket::kReply);
+
+      // Get the enclosing EthernetPacket.
+      EthernetPacket::Ptr eth_pkt =
+          (EthernetPacket*)(pkt->enclosingPacket().ptr());
+
+      // Update the source address to our MAC.
+      eth_pkt->srcIs(iface->mac());
+
+      // Send ARP reply on same interface.
+      cp_->dataPlane()->outputPacketNew(eth_pkt, iface);
+    } else if (pkt->operation() == ARPPacket::kReply) {
+      DLOG << "ARP reply handling unimplemented";
+    }
+  }
 }
 
 
