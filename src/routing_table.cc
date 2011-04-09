21603df715f8d4fa7dee1f0c9557216522915e3c
Ali Yahya <ali01@ali01.com>
2011-04-09 15:13:00 -0700

    Implementation of RoutingTable completed.
    Not yet tested.

--- src/routing_table.cc
+++ src/routing_table.cc
@@ -1,11 +1,74 @@
 #include "routing_table.h"
 
+/* RoutingTable::Entry */
+
+RoutingTable::Entry::Entry(const IPv4Addr& dest,
+                           const IPv4Addr& subnet_mask,
+                           const IPv4Addr& gateway,
+                           Interface::Ptr interface)
+    : subnet_(dest & subnet_mask),
+      subnet_mask_(subnet_mask),
+      gateway_(gateway),
+      interface_(interface),
+      next_(NULL), prev_(NULL) {}
+
+void
+RoutingTable::Entry::subnetIs(const IPv4Addr& dest_ip,
+                              const IPv4Addr& subnet_mask) {
+  subnet_ = dest_ip & subnet_mask;
+  subnet_mask_ = subnet_mask;
+}
+
+/* RoutingTable */
+
 RoutingTable::Entry::Ptr
-RoutingTable::longestPrefixMatch(const IPv4Addr& dest_ip) {
-  return NULL;
+RoutingTable::longestPrefixMatch(const IPv4Addr& dest_ip) const {
+  Entry::Ptr lpm = NULL;
+  Entry::Ptr curr = rtable_;
+  while (curr)  {
+    if (curr->subnet() == (dest_ip & curr->subnetMask())) {
+      if (lpm == NULL || curr->subnetMask() > lpm->subnetMask())
+        lpm = curr;
+    }
+    curr = curr->next_;
+  }
+
+  return lpm;
 }
 
 void
 RoutingTable::entryIs(Entry::Ptr entry) {
-  
+  if (entry == NULL)
+    return;
+
+  /* If entry is already in the routing table. */
+  if (rtable_ && rtable_set_.find(entry.ptr()) != rtable_set_.end())
+    return;
+
+  rtable_set_.insert(entry.ptr());
+
+  if (rtable_)
+    rtable_->prev_ = entry.ptr();
+
+  entry->prev_ = NULL;
+  entry->next_ = rtable_;
+  rtable_ = entry;
+}
+
+void
+RoutingTable::entryDel(Entry::Ptr entry) {
+  if (entry == NULL || rtable_ == NULL)
+    return;
+
+  /* If entry is the root node. */
+  if (rtable_ == entry)
+    rtable_ = entry->next_;
+  else
+    entry->prev_->next_ = entry->next_;
+
+  /* If entry is not the tail. */
+  if (entry->next_)
+    entry->next_->prev_ = entry->prev_;
+
+  rtable_set_.erase(entry.ptr());
 }
--- src/routing_table.h
+++ src/routing_table.h
@@ -1,9 +1,13 @@
 #ifndef ROUTING_TABLE_H_HE9H7VS9
 #define ROUTING_TABLE_H_HE9H7VS9
 
+#include <set>
+
 #include "fwk/ptr_interface.h"
 
 #include "ip_packet.h"
+#include "interface.h"
+
 
 class RoutingTable : public Fwk::PtrInterface<RoutingTable> {
  public:
@@ -16,14 +20,40 @@ class RoutingTable : public Fwk::PtrInterface<RoutingTable> {
     typedef Fwk::Ptr<const Entry> PtrConst;
     typedef Fwk::Ptr<Entry> Ptr;
 
-    static Ptr EntryNew() {
-      return new Entry();
+    static Ptr EntryNew(const IPv4Addr& dest,
+                        const IPv4Addr& subnet_mask,
+                        const IPv4Addr& gateway,
+                        Interface::Ptr interface) {
+      return new Entry(dest, subnet_mask, gateway, interface);
     }
 
+    IPv4Addr subnet() const { return subnet_; }
+    IPv4Addr subnetMask() const { return subnet_mask_; }
+    IPv4Addr gateway() const { return gateway_; }
+    Interface::Ptr interface() const { return interface_; }
+
+    void subnetIs(const IPv4Addr& dest, const IPv4Addr& subnet_mask);
+    void gatewayIs(const IPv4Addr& gateway) { gateway_ = gateway; }
+    void interfaceIs(Interface::Ptr iface) { interface_ = iface; }
+
    protected:
-    Entry() {}
+    Entry (const IPv4Addr& dest,
+           const IPv4Addr& subnet_mask,
+           const IPv4Addr& gateway,
+           Interface::Ptr interface);
 
    private:
+    IPv4Addr subnet_;
+    IPv4Addr subnet_mask_;
+    IPv4Addr gateway_;
+    Interface::Ptr interface_;
+
+    /* Linked list support. */
+    Entry::Ptr next_;
+    Entry* prev_; /* weak pointer to prevent circular references */
+
+    friend class RoutingTable;
+
     /* Operations disallowed */
     Entry(const Entry&);
     void operator=(const Entry&);
@@ -33,14 +63,19 @@ class RoutingTable : public Fwk::PtrInterface<RoutingTable> {
     return new RoutingTable();
   }
 
-  Entry::Ptr longestPrefixMatch(const IPv4Addr& dest_ip);
+  Entry::Ptr longestPrefixMatch(const IPv4Addr& dest_ip) const;
 
   void entryIs(Entry::Ptr entry);
+  void entryDel(Entry::Ptr entry);
 
  protected:
   RoutingTable() {}
 
  private:
+  /* Routing table is a linked list. */
+  Entry::Ptr rtable_; /* Convert to a radix trie if time allows. */
+  std::set<Entry*> rtable_set_; /* Prevents double insertion of an entry */
+
   /* Operations disallowed. */
   RoutingTable(const RoutingTable&);
   void operator=(const RoutingTable&);
