3b5d7e3045b0ac30c8e625ad7f75987bd07731c5
Ali Yahya <ali01@ali01.com>
2011-04-11 14:42:46 -0700

    Testing RoutingTable: basic + deletion.

--- tests/routing_table_unittest.cc
+++ tests/routing_table_unittest.cc
@@ -22,4 +22,36 @@ class RoutingTableTest : public ::testing::Test {
 
 TEST_F(RoutingTableTest, basic) {
   ASSERT_TRUE(routing_table_->lpm("192.168.0.1") == NULL);
+
+  routing_table_->entryIs(entry_one_);
+  routing_table_->entryIs(entry_two_);
+  routing_table_->entryIs(entry_three_);
+
+  RoutingTable::Entry::Ptr entry = routing_table_->lpm("202.18.49.17");
+  ASSERT_TRUE(entry != NULL);
+  EXPECT_EQ(entry->subnet(), IPv4Addr("202.18.0.0"));
+
+  entry = routing_table_->lpm("202.49.29.19");
+  ASSERT_TRUE(entry != NULL);
+  EXPECT_EQ(entry->subnet(), IPv4Addr("202.0.0.0"));
+
+  entry = routing_table_->lpm("184.72.19.250");
+  ASSERT_TRUE(entry != NULL);
+  EXPECT_EQ(entry->subnet(), IPv4Addr("184.72.19.0"));
+}
+
+TEST_F(RoutingTableTest, deletion) {
+  RoutingTable::Entry::Ptr entry;
+  routing_table_->entryIs(entry_one_);
+  routing_table_->entryIs(entry_two_);
+  routing_table_->entryIs(entry_three_);
+
+  routing_table_->entryDel(entry_one_);
+  entry = routing_table_->lpm("184.72.19.250");
+  EXPECT_TRUE(entry == NULL);
+
+  routing_table_->entryDel(entry_two_);
+  entry = routing_table_->lpm("202.18.81.49");
+  ASSERT_TRUE(entry != NULL);
+  EXPECT_EQ(entry->subnet(), IPv4Addr("202.0.0.0"));
 }
