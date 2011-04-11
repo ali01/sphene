246e0f66140d5bdbd3a25227f67ee2e69a2bfb1c
Matt Sparks <ms@quadpoint.org>
2011-04-11 15:56:10 -0700

    ICMP packet implementation

---
+++ src/icmp_packet.cc
@@ -0,0 +1,83 @@
+#include "icmp_packet.h"
+
+#include <arpa/inet.h>
+#include <cstring>
+#include <inttypes.h>
+#include <string>
+#include "fwk/buffer.h"
+#include "fwk/exception.h"
+#include "interface.h"
+#include "ip_packet.h"
+
+
+ICMPPacket::ICMPPacket(Fwk::Buffer::Ptr buffer, unsigned int buffer_offset)
+    : Packet(buffer, buffer_offset),
+      icmp_hdr_((struct ICMPHeader *)offsetAddress(0)) { }
+
+
+void ICMPPacket::operator()(Functor* const f, const Interface::PtrConst iface) {
+  (*f)(this, iface);
+}
+
+
+ICMPPacket::Type ICMPPacket::type() const {
+  return (Type)(icmp_hdr_->type);
+}
+
+
+void ICMPPacket::typeIs(const Type t) {
+  uint8_t tn = t;
+  icmp_hdr_->type = tn;
+}
+
+
+uint8_t ICMPPacket::code() const {
+  return icmp_hdr_->code;
+}
+
+
+void ICMPPacket::codeIs(const uint8_t code) {
+  icmp_hdr_->code = code;
+}
+
+
+uint16_t ICMPPacket::checksum() const {
+  return ntohs(icmp_hdr_->cksum);
+}
+
+
+void ICMPPacket::checksumIs(const uint16_t ck) {
+  icmp_hdr_->cksum = htons(ck);
+}
+
+
+void ICMPPacket::checksumReset() {
+  checksumIs(0);
+  checksumIs(computeChecksum());
+}
+
+
+// TODO(ms): This should be moved to a util.h/util.cc pair.
+// TODO(ms): This is not correct. Checksums in ICMP are calculated over the
+//   header and data, which is not yet part of the above implementation.
+uint16_t ICMPPacket::computeChecksum() const {
+  uint32_t sum;
+  unsigned int len = sizeof(struct ICMPHeader);
+  const uint8_t *header = (const uint8_t *)icmp_hdr_;
+
+  for (sum = 0; len >= 2; header += 2, len -= 2)
+    sum += header[0] << 8 | header[1];
+
+  if (len > 0)
+    sum += header[0] << 8;
+
+  while (sum > 0xFFFF)
+    sum = (sum >> 16) + (sum & 0xFFFF);
+
+  sum = ~sum;
+
+  if (sum == 0)
+    sum = 0xFFFF;
+
+  return sum;
+}
--- src/icmp_packet.h
+++ src/icmp_packet.h
@@ -13,19 +13,55 @@ class ICMPPacket : public Packet {
   typedef Fwk::Ptr<const ICMPPacket> PtrConst;
   typedef Fwk::Ptr<ICMPPacket> Ptr;
 
+  enum Type {
+    kEchoReply = 0,
+    kDestUnreachable = 3,
+    kEchoRequest = 8,
+    kTimeExceeded = 11
+  };
+
   static Ptr ICMPPacketNew(Fwk::Buffer::Ptr buffer,
                            unsigned int buffer_offset) {
     return new ICMPPacket(buffer, buffer_offset);
   }
 
-  /* Double-dispatch support. */
-  virtual void operator()(Functor* f, Fwk::Ptr<const Interface> iface) {
-    (*f)(this, iface);
-  }
+  // Double-dispatch support.
+  virtual void operator()(Functor* f, Fwk::Ptr<const Interface> iface);
+
+  // Returns the ICMP Type.
+  Type type() const;
+
+  // Sets the ICMP Type.
+  void typeIs(Type t);
+
+  // Returns the ICMP Code (subtype).
+  uint8_t code() const;
+
+  // Sets the ICMP Code (subtype).
+  void codeIs(uint8_t code);
+
+  // Returns ICMP header+data checksum.
+  uint16_t checksum() const;
+
+  // Sets the ICMP header+data checksum.
+  void checksumIs(uint16_t ck);
+
+  // Recomputes the checksum of the ICMP header+data.
+  void checksumReset();
 
  protected:
-  ICMPPacket(Fwk::Buffer::Ptr buffer, unsigned int buffer_offset)
-      : Packet(buffer, buffer_offset) {}
+  ICMPPacket(Fwk::Buffer::Ptr buffer, unsigned int buffer_offset);
+
+  uint16_t computeChecksum() const;
+
+ private:
+  struct ICMPHeader {
+    uint8_t type;
+    uint8_t code;
+    uint16_t cksum;
+  } __attribute__((packed));
+
+  struct ICMPHeader* icmp_hdr_;
 };
 
 #endif
