a93d1352f021e8cefb33a2ff23840c3190935c8e
Matt Sparks <ms@quadpoint.org>
2011-04-11 17:22:23 -0700

    Simple ICMP packet and tests

--- tests/icmp_packet_unittest.cc
+++ tests/icmp_packet_unittest.cc
@@ -27,8 +27,19 @@ class ICMPPacketTest : public ::testing::Test {
  protected:
   virtual void SetUp() {
     // Everything here is in NBO.
-    uint8_t icmp_packet[] = { 0xFF, 0xFF, 0xFF, 0xFF,
-                              0xFF, 0xFF, 0xFF, 0xFF };  // TODO(ms): this.
+    // Type 8 code 0 is a ping request.
+    uint8_t icmp_packet[] = { 0x08,        // Type
+                              0x00,        // Code
+                              0x1D, 0xEF,  // Checksum
+                              0x72, 0x70,  // Identifier
+                              0x00, 0x00,  // Sequence number
+                              0x4D, 0xA3, 0x95, 0xD4, 0x00, 0x08, 0x99, 0x1D,
+                              0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+                              0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+                              0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+                              0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+                              0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
+                              0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37 };
 
     // Put a packet in a buffer.
     buf_ = Fwk::Buffer::BufferNew(icmp_packet, sizeof(icmp_packet));
@@ -47,20 +58,17 @@ class ICMPPacketTest : public ::testing::Test {
 
 
 TEST_F(ICMPPacketTest, type) {
-
-
+  EXPECT_EQ(header_->type, pkt_->type());
 }
 
 
 TEST_F(ICMPPacketTest, code) {
-
-
+  EXPECT_EQ(header_->code, pkt_->code());
 }
 
 
 TEST_F(ICMPPacketTest, checksum) {
-
-
+  EXPECT_EQ(ntohs(header_->cksum), pkt_->checksum());
 }
 
 
