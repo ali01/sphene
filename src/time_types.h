d413f6b5b95edb0480826fe695f5bc4b788ed417
Matt Sparks <ms@quadpoint.org>
2011-04-20 19:21:31 -0700

    Fleshing out time types

--- src/time_types.h
+++ src/time_types.h
@@ -7,7 +7,15 @@
 #include "fwk/ordinal.h"
 
 
-class Seconds : public Fwk::Ordinal<Seconds, uint32_t> { };
-class TimeEpoch : public Fwk::Ordinal<TimeEpoch, time_t> { };
+class Seconds : public Fwk::Ordinal<Seconds, uint32_t> {
+ public:
+  Seconds(const uint32_t s) : Ordinal<Seconds, uint32_t>(s) { }
+};
+
+
+class TimeEpoch : public Fwk::Ordinal<TimeEpoch, time_t> {
+ public:
+  TimeEpoch(const time_t t) : Ordinal<TimeEpoch, time_t>(t) { }
+};
 
 #endif
