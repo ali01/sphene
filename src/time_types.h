6ad3e635a316e5da437d9acbb6bf6f8fdd006a88
Matt Sparks <ms@quadpoint.org>
2011-04-21 13:47:03 -0700

    Adding TimeDelta type

--- src/time_types.h
+++ src/time_types.h
@@ -7,15 +7,26 @@
 #include "fwk/ordinal.h"
 
 
-class Seconds : public Fwk::Ordinal<Seconds, uint32_t> {
+class Seconds : public Fwk::Ordinal<Seconds, int32_t> {
  public:
-  Seconds(const uint32_t s) : Ordinal<Seconds, uint32_t>(s) { }
+  Seconds(const int32_t s) : Ordinal<Seconds, int32_t>(s) { }
+};
+
+
+class TimeDelta : public Fwk::Ordinal<TimeDelta, time_t> {
+ public:
+  TimeDelta(const time_t d) : Ordinal<TimeDelta, time_t>(d) { }
+  operator Seconds() const { return Seconds(value()); }
 };
 
 
 class TimeEpoch : public Fwk::Ordinal<TimeEpoch, time_t> {
  public:
   TimeEpoch(const time_t t) : Ordinal<TimeEpoch, time_t>(t) { }
+  TimeDelta operator-(const TimeEpoch& other) const {
+    return TimeDelta(value() - other.value());
+  }
+  operator Seconds() const { return Seconds(value()); }
 };
 
 #endif
