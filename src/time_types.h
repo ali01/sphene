25b3efec23385307daa1a3f6e40036bb5f853cf1
Ali Yahya <ali01@ali01.com>
2011-04-23 12:35:01 -0700

    Fixed compiler error in time_types.h

--- src/time_types.h
+++ src/time_types.h
@@ -9,20 +9,20 @@
 
 class Seconds : public Fwk::Ordinal<Seconds, int32_t> {
  public:
-  Seconds(const int32_t s) : Ordinal<Seconds, int32_t>(s) { }
+  Seconds(const int32_t s) : Fwk::Ordinal<Seconds, int32_t>(s) { }
 };
 
 
 class TimeDelta : public Fwk::Ordinal<TimeDelta, time_t> {
  public:
-  TimeDelta(const time_t d) : Ordinal<TimeDelta, time_t>(d) { }
+  TimeDelta(const time_t d) : Fwk::Ordinal<TimeDelta, time_t>(d) { }
   operator Seconds() const { return Seconds(value()); }
 };
 
 
 class TimeEpoch : public Fwk::Ordinal<TimeEpoch, time_t> {
  public:
-  TimeEpoch(const time_t t) : Ordinal<TimeEpoch, time_t>(t) { }
+  TimeEpoch(const time_t t) : Fwk::Ordinal<TimeEpoch, time_t>(t) { }
   TimeDelta operator-(const TimeEpoch& other) const {
     return TimeDelta(value() - other.value());
   }
