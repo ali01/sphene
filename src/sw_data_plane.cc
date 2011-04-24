df22125f83600da20d63a80299c60495670b4585
Matt Sparks <ms@quadpoint.org>
2011-04-24 04:32:27 -0700

    Asterisk associativity

--- src/sw_data_plane.cc
+++ src/sw_data_plane.cc
@@ -8,7 +8,7 @@
 struct sr_instance;
 
 
-SWDataPlane::SWDataPlane(struct sr_instance *sr,
+SWDataPlane::SWDataPlane(struct sr_instance* sr,
                          RoutingTable::Ptr routing_table,
                          ARPCache::Ptr arp_cache)
     : DataPlane("SWDataPlane", sr, routing_table, arp_cache),
