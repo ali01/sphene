cefcc85d5d31afb429d52ad4f79354d70430bd5e
Ali Yahya <ali01@ali01.com>
2011-04-25 02:41:21 -0700

    Moved IPv4Addr to a file of it's own.

--- src/ip_packet.cc
+++ src/ip_packet.cc
@@ -25,65 +25,6 @@ struct ip_hdr {
 const size_t
 IPPacket::kHeaderSize = sizeof(struct ip_hdr);
 
-/* IPv4Addr */
-
-IPv4Addr::IPv4Addr() : Fwk::Ordinal<IPv4Addr,uint32_t>(0) {}
-
-IPv4Addr::IPv4Addr(uint32_t addr) : Fwk::Ordinal<IPv4Addr,uint32_t>(addr) {}
-
-IPv4Addr::IPv4Addr(const std::string& addr) {
-  uint32_t nbo;
-  if (inet_pton(AF_INET, addr.c_str(), &nbo))
-    valueIs(ntohl(nbo));
-  else
-    valueIs(0);
-}
-
-IPv4Addr::IPv4Addr(const char* const addr) {
-  uint32_t nbo;
-  if (inet_pton(AF_INET, addr, &nbo))
-    valueIs(ntohl(nbo));
-  else
-    valueIs(0);
-}
-
-IPv4Addr
-IPv4Addr::operator&(uint32_t other) const {
-  return value() & other;
-}
-
-IPv4Addr
-IPv4Addr::operator&(const IPv4Addr& other) const {
-  return value() & other.value();
-}
-
-IPv4Addr&
-IPv4Addr::operator&=(uint32_t other) {
-  valueIs(value() & other);
-  return *this;
-}
-
-IPv4Addr&
-IPv4Addr::operator&=(const IPv4Addr& other) {
-  valueIs(value() & other.value());
-  return *this;
-}
-
-/* Returns IP address in network byte order */
-uint32_t
-IPv4Addr::nbo() const {
-  return htonl(value());
-}
-
-IPv4Addr::operator std::string() const {
-  uint32_t nbo = this->nbo();
-  char buf[INET_ADDRSTRLEN + 1];
-  inet_ntop(AF_INET, (struct in_addr*)&nbo, buf, sizeof(buf));
-  buf[INET_ADDRSTRLEN] = 0;
-  return buf;
-}
-
-
 
 /* IPPacket */
 
