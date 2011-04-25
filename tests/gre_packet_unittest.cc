f010c3edb487df2593f327220abe4de4e170812f
Matt Sparks <ms@quadpoint.org>
2011-04-25 00:40:37 -0700

    More checksum tests

--- tests/gre_packet_unittest.cc
+++ tests/gre_packet_unittest.cc
@@ -30,7 +30,7 @@ class GREPacketTest : public ::testing::Test {
     uint8_t gre_packet[] = { 0x80,          // Checksum is present
                              0x00,          // Version = 0
                              0x08, 0x00,    // Ptype = IP
-                             0xCC, 0xCC,    // Checksum (currently bogus)
+                             0x77, 0xFF,    // Checksum
                              0x00, 0x00 };  // Reserved1
 
     // Put a packet in a buffer.
@@ -66,4 +66,49 @@ TEST_F(GREPacketTest, checksumPresent) {
 
   // Checksum should not have changed after both operations.
   EXPECT_EQ(ck, pkt_->checksumReset());
+}
+
+
+TEST_F(GREPacketTest, checksum) {
+  // Retrieve checksum from packet.
+  uint16_t expected = ntohs(header_->cksum);
+  EXPECT_EQ(expected, pkt_->checksum());
+
+  // Change the checksum.
+  pkt_->checksumIs(0xDEAD);
+  EXPECT_EQ(0xDEAD, pkt_->checksum());
+
+  // Reset the checksum. Only the checksum field should have changed, so the
+  // checksum should reset back to what we expect.
+  EXPECT_EQ(expected, pkt_->checksumReset());
+
+  // Checksum is essentially 0 if it is not present.
+  pkt_->checksumPresentIs(false);
+  EXPECT_EQ(0, pkt_->checksum());
+}
+
+
+TEST_F(GREPacketTest, checksumValid) {
+  // Check validity of checksum.
+  EXPECT_TRUE(pkt_->checksumValid());
+
+  // Modify checksum to bogus value.
+  pkt_->checksumIs(0xDEAD);
+  EXPECT_FALSE(pkt_->checksumValid());
+
+  // Say the checksum isn't present. Checksum is now automatically valid.
+  pkt_->checksumPresentIs(false);
+  EXPECT_TRUE(pkt_->checksumValid());
+}
+
+
+TEST_F(GREPacketTest, checksumReset) {
+  uint16_t expected = pkt_->checksum();
+
+  // Resetting the checksum should not change the checksum.
+  EXPECT_EQ(expected, pkt_->checksumReset());
+
+  // If the checksum isn't present, resetting is a no-op.
+  pkt_->checksumPresentIs(false);
+  EXPECT_EQ(0, pkt_->checksumReset());
 }
