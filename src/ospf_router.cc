51f88f47746577d2ba268866d045b94f7099c043
Ali Yahya <ali01@ali01.com>
2011-04-25 23:56:00 -0700

    Implemented Hello packet processing logic.

--- src/ospf_router.cc
+++ src/ospf_router.cc
@@ -2,9 +2,11 @@
 
 #include "interface.h"
 
-OSPFRouter::OSPFRouter() :
+OSPFRouter::OSPFRouter(uint32_t router_id, uint32_t area_id) :
   log_(Fwk::Log::LogNew("OSPFRouter")),
-  functor_(this) {}
+  functor_(this),
+  router_id_(router_id),
+  area_id_(area_id) {}
 
 void
 OSPFRouter::packetNew(Packet::Ptr pkt, Interface::PtrConst iface) {
@@ -16,7 +18,9 @@ OSPFRouter::packetNew(Packet::Ptr pkt, Interface::PtrConst iface) {
 /* OSPFRouter::PacketFunctor */
 
 OSPFRouter::PacketFunctor::PacketFunctor(OSPFRouter* ospf_router)
-    : ospf_router_(ospf_router), log_(ospf_router->log_) {}
+    : ospf_router_(ospf_router),
+      neighbors_(&ospf_router->neighbors_),
+      log_(ospf_router->log_) {}
 
 void
 OSPFRouter::PacketFunctor::operator()(OSPFPacket* pkt,
@@ -29,7 +33,57 @@ OSPFRouter::PacketFunctor::operator()(OSPFPacket* pkt,
 void
 OSPFRouter::PacketFunctor::operator()(OSPFHelloPacket* pkt,
                                       Interface::PtrConst iface) {
+  /* Packet validation. */
+  if (!pkt->valid()) {
+    DLOG << "Ignoring invalid packet.";
+    return;
+  }
 
+  if (ospf_router_->areaID() != pkt->areaID()) {
+    DLOG << "Ignoring packet: Area ID doesn't match that of this router.";
+    return;
+  }
+
+  if (iface->subnetMask() != pkt->subnetMask()) {
+    DLOG << "Ignoring packet: Subnet mask does not match "
+         << "that of receiving interface";
+    return;
+  }
+
+  // TODO(ali): Interfaces must be manually removed from OSPFRouter if they
+  //   cease to exist. One approach could be to make use of notifications.
+  OSPFInterfaceDesc::Ptr ifd;
+  ifd = neighbors_->interfaceDesc(iface->ip());
+
+  if (ifd) {
+    if (ifd->helloint() != pkt->helloint()) {
+      DLOG << "Ignoring packet: helloint does not match that of "
+           << "receiving interface";
+      return;
+    }
+
+    uint32_t neighbor_id = pkt->routerID();
+    OSPFNeighbor::Ptr neighbor = ifd->neighbor(neighbor_id);
+
+    if (neighbor == NULL) {
+      /* Packet was sent by a new neighbor.
+       * Creating neighbor object and adding it to the interface description */
+      IPPacket::Ptr ip_pkt = Ptr::st_cast<IPPacket>(pkt->enclosingPacket());
+      IPv4Addr neighbor_addr = ip_pkt->src();
+      neighbor = OSPFNeighbor::New(neighbor_id, neighbor_addr);
+      ifd->neighborIs(neighbor);
+    }
+
+    neighbor->ageIs(0);
+
+  } else {
+    /* Packet was received on an interface that the dynamic router
+     * was unaware about -- possibly a newly created virtual interface.
+     * Creating a new interface description object and
+     * adding it to the neighbor map. */
+    ifd = OSPFInterfaceDesc::New(iface, kDefaultHelloInterval);
+    neighbors_->interfaceDescIs(ifd);
+  }
 }
 
 void
