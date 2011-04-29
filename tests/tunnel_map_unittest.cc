3d9db12c7393f26fff588c7a2a4c4d31d01a253f
Matt Sparks <ms@quadpoint.org>
2011-04-28 18:39:51 -0700

    Adding TunnelMap unittest

---
+++ tests/tunnel_map_unittest.cc
@@ -0,0 +1,65 @@
+#include "gtest/gtest.h"
+
+#include <string>
+#include "ethernet_packet.h"
+#include "interface.h"
+#include "ipv4_addr.h"
+#include "tunnel.h"
+#include "tunnel_map.h"
+
+using std::string;
+
+
+class TunnelMapTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    map_ = TunnelMap::New();
+    eth0_ = Interface::InterfaceNew("eth0");
+    eth0_->ipIs("8.8.4.4");
+    tun0_ = Tunnel::New(eth0_);
+    tun0_->remoteIs("192.168.1.1");
+
+    eth1_ = Interface::InterfaceNew("eth1");
+    eth1_->ipIs("4.2.2.1");
+    tun1_ = Tunnel::New(eth1_);
+    tun1_->remoteIs("10.0.0.1");
+  }
+
+  Interface::Ptr eth0_;
+  Tunnel::Ptr tun0_;
+  Interface::Ptr eth1_;
+  Tunnel::Ptr tun1_;
+  TunnelMap::Ptr map_;
+};
+
+
+TEST_F(TunnelMapTest, Insert) {
+  // Map is initially empty.
+  EXPECT_EQ((size_t)0, map_->tunnels());
+
+  // Insert tunnels into the map.
+  map_->tunnelIs(tun0_);
+  map_->tunnelIs(tun1_);
+
+  // Ensure the map size has increased.
+  EXPECT_EQ((size_t)2, map_->tunnels());
+}
+
+
+TEST_F(TunnelMapTest, Retrieve) {
+  // Insert tunnels.
+  map_->tunnelIs(tun0_);
+  map_->tunnelIs(tun1_);
+
+  // Ensure we can retrieve them by name.
+  EXPECT_EQ(tun0_, map_->tunnel(tun0_->name()));
+  EXPECT_EQ(tun1_, map_->tunnel(tun1_->name()));
+
+  // Ensure we can retrieve them by remote IP address.
+  EXPECT_EQ(tun0_, map_->tunnelRemoteAddr(tun0_->remote()));
+  EXPECT_EQ(tun1_, map_->tunnelRemoteAddr(tun1_->remote()));
+
+  // NULL pointers are returned for non-existent tunnels.
+  Tunnel::Ptr null(NULL);
+  EXPECT_EQ(null, map_->tunnel("bogusiface"));
+}
