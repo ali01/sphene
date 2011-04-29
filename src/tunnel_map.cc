c696b402eb2f56be2ab28e4afe0e8f6faa4dca78
Matt Sparks <ms@quadpoint.org>
2011-04-28 18:21:37 -0700

    Adding TunnelMap

---
+++ src/tunnel_map.cc
@@ -0,0 +1,32 @@
+#include "tunnel_map.h"
+
+#include <string>
+#include <map>
+
+#include "ipv4_addr.h"
+#include "fwk/exception.h"
+#include "tunnel.h"
+
+using std::string;
+
+
+void TunnelMap::tunnelIs(const Tunnel::Ptr tunnel) {
+  if (!tunnel)
+    throw Fwk::ResourceException("TunnelMap::tunnelIs",
+                                 "Tunnel is NULL");
+
+  name_t_map_[tunnel->name()] = tunnel;
+  ip_t_map_[tunnel->remote()] = tunnel;
+}
+
+
+Tunnel::Ptr TunnelMap::tunnel(const string& name) {
+  NameTunnelMap::iterator it = name_t_map_.find(name);
+  return (it != name_t_map_.end()) ? it->second : NULL;
+}
+
+
+Tunnel::Ptr TunnelMap::tunnelRemoteAddr(const IPv4Addr& addr) {
+  IPTunnelMap::iterator it = ip_t_map_.find(addr);
+  return (it != ip_t_map_.end()) ? it->second : NULL;
+}
---
+++ src/tunnel_map.h
@@ -0,0 +1,57 @@
+#ifndef TUNNEL_MAP_H_
+#define TUNNEL_MAP_H_
+
+#include <map>
+#include <string>
+
+#include "ipv4_addr.h"
+#include "fwk/ptr.h"
+#include "fwk/ptr_interface.h"
+#include "tunnel.h"
+
+
+class TunnelMap : public Fwk::PtrInterface<TunnelMap> {
+ public:
+  typedef Fwk::Ptr<const TunnelMap> PtrConst;
+  typedef Fwk::Ptr<TunnelMap> Ptr;
+  typedef std::map<std::string, Tunnel::Ptr> NameTunnelMap;
+  typedef std::map<IPv4Addr, Tunnel::Ptr> IPTunnelMap;
+  typedef NameTunnelMap::iterator iterator;
+  typedef NameTunnelMap::const_iterator const_iterator;
+
+  static Ptr New() {
+    return new TunnelMap();
+  }
+
+  // Adds a new tunnel to the map. If a tunnel with the same name already
+  // exists, it is replaced with 'iface'.
+  void tunnelIs(Tunnel::Ptr tunnel);
+
+  // Returns a tunnel by name 'name'. Returns NULL if no tunnel exists by that
+  // name.
+  Tunnel::Ptr tunnel(const std::string& name);
+
+  // Returns a tunnel with remote IP address 'addr'. Returns NULL if no tunnel
+  // exists with that remote address.
+  Tunnel::Ptr tunnelRemoteAddr(const IPv4Addr& addr);
+
+  // Returns the number of tunnels in the map.
+  size_t tunnels() const { return name_t_map_.size(); }
+
+  iterator begin() { return name_t_map_.begin(); }
+  iterator end() { return name_t_map_.end(); }
+  const_iterator begin() const { return name_t_map_.begin(); }
+  const_iterator end() const { return name_t_map_.end(); }
+
+ protected:
+  TunnelMap() { }
+
+ private:
+  NameTunnelMap name_t_map_;
+  IPTunnelMap ip_t_map_;
+
+  TunnelMap(const TunnelMap&);
+  void operator=(const TunnelMap&);
+};
+
+#endif
