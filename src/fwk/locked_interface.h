e0d0dfad9a62fb578a39fe8190d4f9270cf7cb52
Matt Sparks <ms@quadpoint.org>
2011-04-30 14:53:12 -0700

    Add Fwk::LockedInterface for providing coarse locks

---
+++ src/fwk/locked_interface.h
@@ -0,0 +1,25 @@
+#ifndef FWK_LOCKED_INTERFACE_H_
+#define FWK_LOCKED_INTERFACE_H_
+
+#include <pthread.h>
+
+namespace Fwk {
+
+// LockedInterface provides a coarse lock for a derived class. Derived classes
+// should use Fwk::ScopedLock when possible.
+class LockedInterface {
+ public:
+  void lockedIs(bool locked) {
+    if (locked)
+      pthread_mutex_lock(&li_lock_);
+    else
+      pthread_mutex_unlock(&li_lock_);
+  }
+
+ private:
+  pthread_mutex_t li_lock_;
+};
+
+};
+
+#endif
