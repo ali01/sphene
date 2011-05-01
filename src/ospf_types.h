3271587db09b26657aff568dcd6e4b4fbc07a487
Ali Yahya <ali01@ali01.com>
2011-05-01 16:35:00 -0700

    Added ospf_types.h.

---
+++ src/ospf_types.h
@@ -0,0 +1,11 @@
+/* The purpose of this files existence is to reduce include dependencies.
+ * It allows the inclusion of the enclosed types without having to
+ * include more important files like OSPFRouter. */
+
+#ifndef OSPF_TYPES_H_MUFO34T3
+#define OSPF_TYPES_H_MUFO34T3
+
+typedef uint32_t RouterID;
+typedef uint32_t AreaID;
+
+#endif
