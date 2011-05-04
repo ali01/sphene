ali01/sphene

commit c3fc20075be0aee94a59fa767d8d3152b6ea8b59

Ignore routes on disabled interfaces when doing LPM

src/routing_table.cc
@@ -37,13 +37,16 @@ RoutingTable::RoutingTable() {
 RoutingTable::Entry::Ptr
 RoutingTable::lpm(const IPv4Addr& dest_ip) const {
   Entry::Ptr lpm = NULL;
-  Entry::Ptr curr = rtable_.front();
-  while (curr)  {
-    if (curr->subnet() == (dest_ip & curr->subnetMask())) {
-      if (lpm == NULL || curr->subnetMask() > lpm->subnetMask())
-        lpm = curr;
+  Entry::Ptr entry;
+  for (entry = rtable_.front(); entry; entry = entry->next()) {
+    // Routes on disabled interfaces are ignored; packets can't go out them.
+    if (!entry->interface()->enabled())
+      continue;
+
+    if (entry->subnet() == (dest_ip & entry->subnetMask())) {
+      if (lpm == NULL || entry->subnetMask() > entry->subnetMask())
+        lpm = entry;
     }
-    curr = curr->next();
   }
 
   return lpm;

commit a8568d3c0f4ec5b3962e2107b97f0fbf1d052e64

Fixing insidious bug introduced in c3fc20075be0aee94a59fa767d8d3152b6ea8b59

src/routing_table.cc
@@ -57,7 +57,7 @@ RoutingTable::lpm(const IPv4Addr& dest_ip) {
       continue;
 
     if (entry->subnet() == (dest_ip & entry->subnetMask())) {
-      if (lpm == NULL || entry->subnetMask() > entry->subnetMask())
+      if (lpm == NULL || entry->subnetMask() > lpm->subnetMask())
         lpm = entry;
     }
   }