2dba43ef26ba208d9c1fc2bde7540ad2a637d9cd
Ali Yahya <ali01@ali01.com>
2011-05-06 19:06:09 -0700

    Added ospf_constants: kDefaultLSUInt.

---
+++ src/ospf_constants.cc
@@ -0,0 +1,7 @@
+#include "ospf_constants.h"
+
+namespace OSPF {
+
+const uint8_t kDefaultLSUInt = 30;
+
+} /* end of namespace OSPF */
---
+++ src/ospf_constants.h
@@ -0,0 +1,13 @@
+#ifndef OSPF_CONSTANTS_H_BQNRNDUY
+#define OSPF_CONSTANTS_H_BQNRNDUY
+
+#include <inttypes.h>
+
+namespace OSPF {
+
+/* Interval for OSPF LSU flood updates. */
+extern const uint8_t kDefaultLSUInt;
+
+} /* end of namespace OSPF */
+
+#endif
