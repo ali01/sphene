4050337f49bce02e766926a03a608443e6e0b8c4
Ali Yahya <ali01@ali01.com>
2011-05-08 19:35:54 -0700

    OSPFRouter: Revert "OSPFRouter: Added mutators for routerID and areaID."
    This reverts commit 0b8c8c3ff08aafdfc036dd0cd1cb74ef84026679.

--- src/ospf_router.h
+++ src/ospf_router.h
@@ -74,8 +74,6 @@ class OSPFRouter : public Fwk::PtrInterface<OSPFRouter> {
   /* Mutators. */
 
   void notifieeIs(Notifiee::Ptr _n) { notifiee_ = _n; }
-  void routerIDIs(const RouterID& id) { router_id_ = id; }
-  void areaIDIS(const AreaID& id) { area_id_ = id; }
 
   /* Signals. */
 
@@ -259,7 +257,7 @@ class OSPFRouter : public Fwk::PtrInterface<OSPFRouter> {
   PacketFunctor functor_;
 
   RouterID router_id_;
-  AreaID area_id_;
+  const AreaID area_id_;
   Fwk::Ptr<OSPFNode> router_node_;
   Fwk::Ptr<OSPFInterfaceMap> interfaces_;
   Fwk::Ptr<OSPFTopology> topology_;
