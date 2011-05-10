6df312776616dc7316e7d3b43ad50bd8ea486633
Ali Yahya <ali01@ali01.com>
2011-05-10 06:25:25 -0700

    OSPFTopology[TEST]: Allocated pool of nodes for testing.

--- tests/ospf_topology_unittest.cc
+++ tests/ospf_topology_unittest.cc
@@ -3,28 +3,34 @@
 #include "ospf_node.h"
 #include "ospf_topology.h"
 
+static const unsigned int kNodes = 10;
+
 class OSPFTopologyTest : public ::testing::Test {
  protected:
   OSPFTopologyTest() {
+    for (unsigned int i = 0; i < kNodes; ++i) {
+      nodes_[i] = OSPFNode::New(i + 1);
+    }
+
     root_node_id_ = 0xdeadbeef;
     root_node_ = OSPFNode::New(root_node_id_);
     topology_ = OSPFTopology::New(root_node_);
   }
 
   RouterID root_node_id_;
+  OSPFNode::Ptr nodes_[kNodes];
   OSPFNode::Ptr root_node_;
   OSPFTopology::Ptr topology_;
 };
 
 TEST_F(OSPFTopologyTest, basic) {
-  RouterID nd_id = 0xcafebabe;
-  OSPFNode::Ptr node = OSPFNode::New(nd_id);
   OSPFNode::Ptr temp;
+  OSPFNode::Ptr node = nodes_[0];
 
   topology_->nodeIs(node);
   EXPECT_EQ(topology_->nodes(), (size_t)2);
 
-  temp = topology_->node(nd_id);
+  temp = topology_->node(node->routerID());
   EXPECT_EQ(temp, node);
 
   temp = topology_->node(root_node_id_);
