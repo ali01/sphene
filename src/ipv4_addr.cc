44e6ff76339094c06d9955f5e0ae81bfcbe4442f
Matt Sparks <ms@quadpoint.org>
2011-05-12 21:13:08 -0700

    Make IPv4Addr atomic

--- src/ipv4_addr.cc
+++ src/ipv4_addr.cc
@@ -1,3 +1,4 @@
+#include "fwk/atomic.h"
 #include "ipv4_addr.h"
 
 /* IPv4Addr */
@@ -7,52 +8,85 @@ const IPv4Addr IPv4Addr::kZero = (uint32_t)0x0;
 const int IPv4Addr::kAddrLen;
 
 
-IPv4Addr::IPv4Addr() : Fwk::Ordinal<IPv4Addr,uint32_t>(0) {}
+IPv4Addr::IPv4Addr()
+    : addr_(0) { }
 
-IPv4Addr::IPv4Addr(uint32_t addr) : Fwk::Ordinal<IPv4Addr,uint32_t>(addr) {}
+IPv4Addr::IPv4Addr(uint32_t addr)
+    : addr_(addr) { }
 
 IPv4Addr::IPv4Addr(const std::string& addr) {
   uint32_t nbo;
   if (inet_pton(AF_INET, addr.c_str(), &nbo))
-    valueIs(ntohl(nbo));
+    addr_ = ntohl(nbo);
   else
-    valueIs(0);
+    addr_ = 0;
 }
 
 IPv4Addr::IPv4Addr(const char* const addr) {
   uint32_t nbo;
   if (inet_pton(AF_INET, addr, &nbo))
-    valueIs(ntohl(nbo));
+    addr_ = ntohl(nbo);
   else
-    valueIs(0);
+    addr_ = 0;
 }
 
 IPv4Addr
 IPv4Addr::operator&(uint32_t other) const {
-  return value() & other;
+  return addr_ & other;
 }
 
 IPv4Addr
 IPv4Addr::operator&(const IPv4Addr& other) const {
-  return value() & other.value();
+  return addr_ & other.addr_;
 }
 
 IPv4Addr&
 IPv4Addr::operator&=(uint32_t other) {
-  valueIs(value() & other);
+  addr_ &= other;
   return *this;
 }
 
 IPv4Addr&
 IPv4Addr::operator&=(const IPv4Addr& other) {
-  valueIs(value() & other.value());
+  addr_ &= other.addr_;
   return *this;
 }
+
+bool
+IPv4Addr::operator==(const IPv4Addr& other) const {
+  return (addr_ == other.addr_);
+}
+
+bool
+IPv4Addr::operator!=(const IPv4Addr& other) const {
+  return (addr_ != other.addr_);
+}
+
+bool
+IPv4Addr::operator<=(const IPv4Addr& other) const {
+  return (addr_ <= other.addr_);
+}
+
+bool
+IPv4Addr::operator>=(const IPv4Addr& other) const {
+  return (addr_ >= other.addr_);
+}
+
+bool
+IPv4Addr::operator<(const IPv4Addr& other) const {
+  return (addr_ < other.addr_);
+}
+
+bool
+IPv4Addr::operator>(const IPv4Addr& other) const {
+  return (addr_ > other.addr_);
+}
+
 
 /* Returns IP address in network byte order */
 uint32_t
 IPv4Addr::nbo() const {
-  return htonl(value());
+  return htonl(addr_.value());
 }
 
 IPv4Addr::operator std::string() const {
