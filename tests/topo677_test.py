20b50d0f33d1cef29a402db07af6c291bb47aa81
Ali Yahya <ali01@ali01.com>
2011-06-02 13:22:23 -0700

    topo677_base[TEST]: Doubled helloint.

--- tests/topo677_test.py
+++ tests/topo677_test.py
@@ -28,7 +28,7 @@ def base_dir():
 BINARY = os.getenv('BINARY', os.path.join(base_dir(), 'build', 'sr'))
 REF_BINARY = os.getenv('REF_BINARY', os.path.join(base_dir(), 'sr_ref'))
 
-helloint = 5
+helloint = 10
 
 
 class Router(object):
