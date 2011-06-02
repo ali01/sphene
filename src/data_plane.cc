7a6c2adbf524024c7ad14f4cc8848c97f14f6ce3
Matt Sparks <ms@quadpoint.org>
2011-06-02 12:18:22 -0700

    Reverting ip broadcast ignoring. This breaks for small subnets.

--- src/data_plane.cc
+++ src/data_plane.cc
@@ -196,7 +196,7 @@ void DataPlane::PacketFunctor::operator()(IPPacket* const pkt,
   DLOG << "  src: " << pkt->src();
   DLOG << "  dst: " << pkt->dst();
 
-  if (pkt->dst() == iface->broadcastAddr() || pkt->dst() == IPv4Addr::kMax) {
+  if (pkt->dst() == IPv4Addr::kMax) {
     DLOG << "  ignoring IP broadcast packet";
     return;
   }
